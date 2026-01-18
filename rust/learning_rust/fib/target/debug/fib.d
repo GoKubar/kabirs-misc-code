4e1a98e7d0d6f48b873876d57d96b0c47502dc00
Kabir Goyal <kabirgoyal@icloud.com>
2026-01-18 14:51:15 -0500

    idek

--- rust/learning_rust/fib/target/debug/fib.d
+++ rust/learning_rust/fib/target/debug/fib.d
@@ -1,1 +1,1 @@
-/home/kabir/code/kabirs-misc-code/rust/learning_rust/fib/target/debug/fib: /home/kabir/code/kabirs-misc-code/rust/learning_rust/fib/src/main.rs
+/home/kubar/code/kabirs-misc-code/rust/learning_rust/fib/target/debug/fib: /home/kubar/code/kabirs-misc-code/rust/learning_rust/fib/src/main.rs
